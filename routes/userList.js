442fc98a80a9357f79226c77b360298048c323f8
yulin <2441518624@qq.com>
2019-06-28 11:00:39 +0800

    增删改查接口

--- routes/userList.js
+++ routes/userList.js
@@ -1,24 +1,125 @@
 var express = require('express');
 var router = express.Router();
 
-router.get('/getUserList', function(req, res, next) {
 
-
-
-    // var user = new User();
-    // var params = URL.parse(req.url, true).query;
-    // if(params.id == '1') {
-    //     user.name = "ligh";
-    //     user.age = "1";
-    //     user.city = "北京市";
-    // }else{
-    //     user.name = "SPTING";
-    //     user.age = "1";
-    //     user.city = "杭州市";
-    // }
-    // var response = {status:1,data:user};
-    // res.send(JSON.stringify(response));
-    res.send('ok');
+var http = require("http");
+//引入数据库
+var mysql=require('mysql');
+var url = require('url');
+//实现本地链接
+var pool = mysql.createPool({
+    host: 'localhost',
+    user: 'root',
+    password: 'YU72124009lin%',
+    database: 'first_db',
+    port: '3306'
 });
+
+//默认情况下是不允许执行多条查询语句的。要使用多条查询语句的功能，就需要在创建数据库连接的时候打开这一功能
+mysql.createConnection( { multipleStatements: true } );
+
+function query( sql, values ) {
+    return new Promise(( resolve, reject ) => {
+        pool.getConnection(function(err, connection){
+            connection.query(sql, values, function(err, results){
+                if(err) {
+                    reject( err );
+                } else {
+                    results = JSON.stringify(results);
+                    results = JSON.parse(results);
+                    resolve( results );
+                }
+            });
+            connection.release();
+        });
+    });
+};
+
+router.get('/save', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Content-Type", "application/json; charset=utf-8")
+
+    var reqUrl = req.url;
+    var reqObj  = url.parse(reqUrl,true).query;
+    var name = reqObj.name,
+     sex = reqObj.sex,
+     age = reqObj.age,
+     hobbies = reqObj.hobbies;
+
+    var sql = 'INSERT INTO user SET ' +
+        'name=\'' + name + '\', sex=' + sex +
+        ', age= ' + age + ', hobbies=\'' + hobbies + '\';';
+
+    console.log(JSON.stringify(sql))
+    query(sql, '').then(function (result) {
+        res.end(JSON.stringify(result));
+    },function (error) {
+
+    });
+});
+
+router.get('/delete', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Content-Type", "application/json; charset=utf-8")
+
+    var reqUrl = req.url,
+     reqObj  = url.parse(reqUrl,true).query,
+     id = reqObj.id;
+
+    var sql = 'delete from user where id=' + id;
+
+    console.log(JSON.stringify(sql))
+    query(sql, '').then(function (result) {
+        res.end(JSON.stringify(result));
+    },function (error) {
+
+    });
+});
+
+router.get('/update', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Content-Type", "application/json; charset=utf-8")
+
+    var reqUrl = req.url;
+    var reqObj  = url.parse(reqUrl,true).query;
+    var id = reqObj.id,
+     name = reqObj.name,
+     sex = reqObj.sex,
+     age = reqObj.age,
+     hobbies = reqObj.hobbies;
+
+    var sql = 'UPDATE user SET name=\'' + name +
+        '\', sex= ' + sex +
+        ', age= ' + age +
+        ', hobbies=\'' + hobbies + '\' ' +
+        'where id =' + id + ';';
+
+    console.log(JSON.stringify(sql))
+    query(sql, '').then(function (result) {
+        res.end(JSON.stringify(result));
+    },function (error) {
+
+    });
+});
+
+router.get('/getUserList', function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header("Access-Control-Allow-Headers", "Content-Type,Content-Length, Authorization, Accept,X-Requested-With");
+    res.header("Access-Control-Allow-Methods","PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Content-Type", "application/json; charset=utf-8");
+
+    query('SELECT * FROM user', '').then(function (result) {
+        res.end(JSON.stringify(result));
+    },function (error) {
+
+    });
+});
+
 
 module.exports = router;
